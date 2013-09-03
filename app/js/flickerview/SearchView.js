d78f47838f9791c5f8c0b802deaad67b3bfcec6d
Sebastien Pereira <spereira@javalive.net>
2013-09-03 13:39:29 +0200

    custom views skeletons

---
+++ app/js/flickerview/SearchView.js
@@ -0,0 +1,14 @@
+define([
+	"dojo/_base/declare",
+	"dojox/mobile/ScrollableView"
+], function(declare, ScrollableView){
+
+	return declare("flickerview.SearchView", [ScrollableView], {
+		/**
+		 *
+		 */
+		startup: function() {
+			this.inherited(arguments);
+		}
+	});
+});
---
+++ app/js/flickerview/SettingsView.js
@@ -0,0 +1,14 @@
+define([
+	"dojo/_base/declare",
+	"dojox/mobile/ScrollableView"
+], function(declare, ScrollableView){
+
+	return declare("flickerview.SettingsView", [ScrollableView], {
+		/**
+		 *
+		 */
+		startup: function() {
+			this.inherited(arguments);
+		}
+	});
+});
